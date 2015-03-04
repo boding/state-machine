0319143ba12e28a1a4f8ebbae4e9dadc396714cb
bobding <bobding@bobdings-MacBook-Pro.local>
2015-03-04 22:51:47 +0800

    add

--- State.cs
+++ State.cs
@@ -7,69 +7,66 @@
  *
 ************************************************/
 
-namespace WeFly
+/// <summary>
+/// 状态基类
+/// </summary>
+public class State
 {
     /// <summary>
-    /// 状态基类
+    /// 状态类型
     /// </summary>
-    public class State
+    protected uint mStateType = 0;
+
+    public uint StateType
     {
-        /// <summary>
-        /// 状态类型
-        /// </summary>
-        protected uint mStateType = 0;
+        get { return mStateType; }
+        set { mStateType = value; }
+    }
 
-        public uint StateType
-        {
-            get { return mStateType; }
-            set { mStateType = value; }
-        }
+    /// <summary>
+    /// 初始化状态
+    /// </summary>
+    /// <returns></returns>
+    public virtual bool Initialize()
+    {
+        return true;
+    }
 
-        /// <summary>
-        /// 初始化状态
-        /// </summary>
-        /// <returns></returns>
-        public virtual bool Initialize()
-        {
-            return true;
-        }
+    /// <summary>
+    /// 反初始化状态
+    /// </summary>
+    /// <returns></returns>
+    public virtual bool UnInitialize()
+    {
+        return true;
+    }
 
-        /// <summary>
-        /// 反初始化状态
-        /// </summary>
-        /// <returns></returns>
-        public virtual bool UnInitialize()
-        {
-            return true;
-        }
+    /// <summary>
+    /// 进入状态
+    /// </summary>
+    /// <param name="arg0"></param>
+    /// <param name="arg1"></param>
+    public virtual void Enter(object arg0, object arg1)
+    {
 
-        /// <summary>
-        /// 进入状态
-        /// </summary>
-        /// <param name="arg0"></param>
-        /// <param name="arg1"></param>
-        public virtual void Enter(object arg0, object arg1)
-        {
+    }
 
-        }
+    /// <summary>
+    /// 离开状态
+    /// </summary>
+    /// <param name="arg0"></param>
+    /// <param name="arg1"></param>
+    public virtual void Leave(object arg0, object arg1)
+    {
 
-        /// <summary>
-        /// 离开状态
-        /// </summary>
-        /// <param name="arg0"></param>
-        /// <param name="arg1"></param>
-        public virtual void Leave(object arg0, object arg1)
-        {
+    }
 
-        }
+    /// <summary>
+    /// 状态更新
+    /// </summary>
+    /// <param name="delta"></param>
+    public virtual void Update(float delta)
+    {
 
-        /// <summary>
-        /// 状态更新
-        /// </summary>
-        /// <param name="delta"></param>
-        public virtual void Update(float delta)
-        {
-
-        }
     }
 }
--- StateMachine.cs
+++ StateMachine.cs
@@ -9,172 +9,169 @@
 
 using System.Collections.Generic;
 
-namespace WeFly
+public class StateMachine
 {
-    public class StateMachine
+    /// <summary>
+    /// ×´Ì¬ÁÐ±í
+    /// </summary>
+    protected Dictionary<uint, State> mStates = new Dictionary<uint, State>();
+
+    /// <summary>
+    /// µ±Ç°×´Ì¬
+    /// </summary>
+    protected State mCurrentState;
+
+    public State CurrentState
     {
-        /// <summary>
-        /// ×´Ì¬ÁÐ±í
-        /// </summary>
-        protected Dictionary<uint, State> mStates = new Dictionary<uint, State>();
+        get { return mCurrentState; }
+    }
 
-        /// <summary>
-        /// µ±Ç°×´Ì¬
-        /// </summary>
-        protected State mCurrentState;
-
-        public State CurrentState
+    /// <summary>
+    /// ÅÐ¶Ï×´Ì¬
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public bool IsState(uint type)
+    {
+        if (null != mCurrentState)
         {
-            get { return mCurrentState; }
+            return mCurrentState.StateType == type;
         }
 
-        /// <summary>
-        /// ÅÐ¶Ï×´Ì¬
-        /// </summary>
-        /// <param name="type"></param>
-        /// <returns></returns>
-        public bool IsState(uint type)
+        return false;
+    }
+
+    /// <summary>
+    /// »ñÈ¡×´Ì¬
+    /// </summary>
+    /// <param name="type">×´Ì¬ÀàÐÍ</param>
+    /// <returns></returns>
+    public State GetState(uint type)
+    {
+        State state;
+        if (mStates.TryGetValue(type, out state))
         {
-            if (null != mCurrentState)
-            {
-                return mCurrentState.StateType == type;
-            }
+            return state;
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// ³õÊ¼»¯
+    /// </summary>
+    /// <returns></returns>
+    public virtual bool Initialize()
+    {
+        foreach (State state in mStates.Values)
+        {
+            state.Initialize();
+        }
+
+        return true;
+    }
+
+    /// <summary>
+    /// ·´³õÊ¼»¯
+    /// </summary>
+    /// <returns></returns>
+    public virtual bool UnInitialize()
+    {
+        foreach (State state in mStates.Values)
+        {
+            state.UnInitialize();
+        }
+
+        mStates.Clear();
+
+        return true;
+    }
+
+    /// <summary>
+    /// ¸üÐÂ×´Ì¬
+    /// </summary>
+    /// <param name="delat"></param>
+    public virtual void Update(float delta)
+    {
+        if (null != mCurrentState)
+        {
+            mCurrentState.Update(delta);
+        }
+    }
+
+    /// <summary>
+    /// ÇÐ»»×´Ì¬£¬ÔÊÐí×´Ì¬ÖØÈë£¬¼´ÐÂ¾É×´Ì¬ÏàÍ¬Ê±£¬ÈÔÖ´ÐÐ×´Ì¬ÇÐ»»
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="arg0"></param>
+    /// <param name="arg1"></param>
+    public virtual void RunState(uint type, object arg0, object arg1)
+    {
+        if (null != mCurrentState)
+        {
+            mCurrentState.Leave(type, null);
+            mCurrentState = null;
+        }
+
+        if (mStates.ContainsKey(type))
+        {
+            mCurrentState = mStates[type];
+            mCurrentState.Enter(arg0, arg1);
+        }
+        else
+        {
+            LogHelper.LogError("[StateMachine.RunState] failed, invaid state: " + type);
+        }
+    }
+
+    /// <summary>
+    /// Í£Ö¹µ±Ç°×´Ì¬£¬ÇÐ»»µ½¿Õ×´Ì¬
+    /// </summary>
+    public virtual void StopState()
+    {
+        if (null != mCurrentState)
+        {
+            mCurrentState.Leave(null, null);
+            mCurrentState = null;
+        }
+    }
+
+    /// <summary>
+    /// ×¢²á×´Ì¬
+    /// </summary>
+    /// <param name="type">×´Ì¬ÀàÐÍ</param>
+    /// <param name="state">×´Ì¬ÊµÀý</param>
+    /// <returns></returns>
+    public bool RegisterState(uint type, State state)
+    {
+        if (mStates.ContainsKey(type))
+        {
+            LogHelper.LogError("[StateMachine.RegisterState] failed, state already registered, state: " + type);
 
             return false;
         }
 
-        /// <summary>
-        /// »ñÈ¡×´Ì¬
-        /// </summary>
-        /// <param name="type">×´Ì¬ÀàÐÍ</param>
-        /// <returns></returns>
-        public State GetState(uint type)
-        {
-            State state;
-            if (mStates.TryGetValue(type, out state))
-            {
-                return state;
-            }
+        mStates.Add(type, state);
 
-            return null;
+        return true;
+    }
+
+    /// <summary>
+    /// ·´×¢²á×´Ì¬
+    /// </summary>
+    /// <param name="type">×´Ì¬ÀàÐÍ</param>
+    /// <returns></returns>
+    public bool UnregisterState(uint type)
+    {
+        if (!mStates.ContainsKey(type))
+        {
+            LogHelper.LogError("[StateMachine.UnregisterState] failed, state does not exist, state: " + type);
+
+            return false;
         }
 
-        /// <summary>
-        /// ³õÊ¼»¯
-        /// </summary>
-        /// <returns></returns>
-        public virtual bool Initialize()
-        {
-//             foreach (State state in mStates.Values)
-//             {
-//                 state.Initialize();
-//             }
+        mStates.Remove(type);
 
-            return true;
-        }
-
-        /// <summary>
-        /// ·´³õÊ¼»¯
-        /// </summary>
-        /// <returns></returns>
-        public virtual bool UnInitialize()
-        {
-            foreach (State state in mStates.Values)
-            {
-                state.UnInitialize();
-            }
-
-            mStates.Clear();
-
-            return true;
-        }
-
-        /// <summary>
-        /// ¸üÐÂ×´Ì¬
-        /// </summary>
-        /// <param name="delat"></param>
-        public virtual void Update(float delta)
-        {
-            if (null != mCurrentState)
-            {
-                mCurrentState.Update(delta);
-            }
-        }
-
-        /// <summary>
-        /// ÇÐ»»×´Ì¬£¬ÔÊÐí×´Ì¬ÖØÈë£¬¼´ÐÂ¾É×´Ì¬ÏàÍ¬Ê±£¬ÈÔÖ´ÐÐ×´Ì¬ÇÐ»»
-        /// </summary>
-        /// <param name="type"></param>
-        /// <param name="arg0"></param>
-        /// <param name="arg1"></param>
-        public virtual void RunState(uint type, object arg0, object arg1)
-        {
-            if (null != mCurrentState)
-            {
-                mCurrentState.Leave(type, null);
-                mCurrentState = null;
-            }
-
-            if (mStates.ContainsKey(type))
-            {
-                mCurrentState = mStates[type];
-                mCurrentState.Enter(arg0, arg1);
-            }
-            else
-            {
-                LogHelper.LogError("[StateMachine.RunState] failed, invaid state: " + type);
-            }
-        }
-
-        /// <summary>
-        /// Í£Ö¹µ±Ç°×´Ì¬£¬ÇÐ»»µ½¿Õ×´Ì¬
-        /// </summary>
-        public virtual void StopState()
-        {
-            if (null != mCurrentState)
-            {
-                mCurrentState.Leave(null, null);
-                mCurrentState = null;
-            }
-        }
-
-        /// <summary>
-        /// ×¢²á×´Ì¬
-        /// </summary>
-        /// <param name="type">×´Ì¬ÀàÐÍ</param>
-        /// <param name="state">×´Ì¬ÊµÀý</param>
-        /// <returns></returns>
-        public bool RegisterState(uint type, State state)
-        {
-            if (mStates.ContainsKey(type))
-            {
-                LogHelper.LogError("[StateMachine.RegisterState] failed, state already registered, state: " + type);
-
-                return false;
-            }
-
-            mStates.Add(type, state);
-
-            return true;
-        }
-
-        /// <summary>
-        /// ·´×¢²á×´Ì¬
-        /// </summary>
-        /// <param name="type">×´Ì¬ÀàÐÍ</param>
-        /// <returns></returns>
-        public bool UnregisterState(uint type)
-        {
-            if (!mStates.ContainsKey(type))
-            {
-                LogHelper.LogError("[StateMachine.UnregisterState] failed, state does not exist, state: " + type);
-
-                return false;
-            }
-
-            mStates.Remove(type);
-
-            return true;
-        }
+        return true;
     }
 }
